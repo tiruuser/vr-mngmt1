be6b654aeb8950f04a5e11d5b975529f278591c5
Triveja Potla <triveja.potla@globallogic.com>
2022-10-28 11:12:01 +0530

    My first commit

---
+++ vr-mngmt/src/main/java/poc/versionmanagement/vrmngmt/VrMngmtApplication.java
@@ -0,0 +1,13 @@
+package poc.versionmanagement.vrmngmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrMngmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrMngmtApplication.class, args);
+	}
+
+}
---
+++ vr-mngmt/src/test/java/poc/versionmanagement/vrmngmt/VrMngmtApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.versionmanagement.vrmngmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrMngmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
